5e1571ba323fdd368c2e879e9cfade7e4ce218fb
Arthur Carabott <arthurc@gmail.com>
2017-04-22 22:04:59 +0100

    segment: add debug mode

--- cv/python/dice-segment.py
+++ cv/python/dice-segment.py
@@ -5,13 +5,19 @@ import cv2
 import os
 import argparse
 
+in_path = None
+out_dir = None
+debug = False
 
-def get_output_path(in_path, out_dir, idx):
+
+def get_output_path(key):
+  global in_path, out_dir
   prefix = os.path.splitext(os.path.basename(in_path))[0]
-  return '{}/{}-{}.png'.format(out_dir, prefix, str(idx).zfill(2))
+  return '{}/{}-{}.png'.format(out_dir, prefix, key)
 
 
-def write_img(img, path):
+def write_img(img, key):
+  path = get_output_path(key)
   cv2.imwrite(path, img)
   print('wrote to {}'.format(path))
 
@@ -41,6 +47,10 @@ def process_contour(contour, img):
   center = (width / 2, height / 2)
   dsize = (width, height)
   matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+
+  if 0 in cropped.shape:
+    return
+
   straight = cv2.warpAffine(cropped, matrix, dsize)
 
   # crop based on nonzero values
@@ -62,13 +72,18 @@ def process_contour(contour, img):
   square = np.zeros((max_dim, max_dim))
   square[x_start:x_end, y_start:y_end] = straight_crop
 
+  if 0 in square.shape:
+    return
+
   # resize
   small = cv2.resize(square, (40, 40))
 
   return small
 
 
-def get_segmented(img):
+def get_segmented(img, threshold):
+  global debug
+
   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
   blurred = cv2.blur(img_gray, (20, 20))
@@ -76,7 +91,10 @@ def get_segmented(img):
   # white dice on black
 #     retval, threshold = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)
   # black dice on white
-  retval, threshold = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY_INV)
+  retval, threshold = cv2.threshold(blurred, threshold, 255,
+                                    cv2.THRESH_BINARY_INV)
+  if debug:
+    write_img(threshold, 'threshold')
 
   min_idx = img.shape.index(min(img.shape[0:2]))
   max_idx = abs(1 - min_idx)
@@ -88,6 +106,10 @@ def get_segmented(img):
   resized, contours, hierarchy = cv2.findContours(resized,
                                                   cv2.RETR_EXTERNAL,
                                                   cv2.CHAIN_APPROX_NONE)
+  if debug:
+    cimg = np.zeros(resized.shape)
+    cv2.drawContours(cimg, contours, -1, 255)
+    write_img(cimg, 'contours')
 
   return [process_contour(c, resized) for c in contours]
 
@@ -95,8 +117,11 @@ def get_segmented(img):
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('in_path', help='path to input image', type=str)
-  parser.add_argument('--out_dir', help='directory to output results',
-                      type=str, default='.')
+  parser.add_argument('out_dir', help='directory to output results', type=str)
+  parser.add_argument('--threshold', help='threshold 0-255', type=int,
+                      default=127)
+  parser.add_argument('--debug', help='if True then output debug images',
+                      type=bool, default=False)
   args = parser.parse_args()
 
   img = cv2.imread(args.in_path)
@@ -104,9 +129,14 @@ def main():
     print("couldn't read image")
     return
 
-  for i, segment in enumerate(get_segmented(img)):
-    filename = get_output_path(args.in_path, args.out_dir, i)
-    write_img(segment, filename)
+  global in_path, out_dir, debug
+  in_path = args.in_path
+  out_dir = args.out_dir
+  debug = args.debug
+
+  for i, segment in enumerate(get_segmented(img, args.threshold)):
+    key = str(i).zfill(2)
+    write_img(segment, key)
 
 
 if __name__ == '__main__':

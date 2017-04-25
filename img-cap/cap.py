939503bb3c4b352dac789163cb08cf9d9e3ca7b1
Arthur Carabott <arthurc@gmail.com>
2017-04-25 14:36:01 +0000

    cap is now a function

--- img-cap/cap.py
+++ img-cap/cap.py
@@ -7,52 +7,55 @@ import RPi.GPIO as GPIO
 import time
 import glob
 
-out_dir = 'output'
-prefix = 'dice-'
-
-file_format = 'jpg'
-imgs = glob.glob('{}/*.{}'.format(out_dir, file_format))
-
-
-if len(imgs) == 0:
-  next_num = 0
-else:
-  nums = [int(splitext(basename(img))[0].replace(prefix, '')) for img in imgs]
-  next_num = 1 + max(nums)
-
-out_name = '{}/{}{}.{}'.format(out_dir, prefix, str(next_num).zfill(4), file_format)
-
 #filelist = glob.glob('{}/*.{}'.format(out_dir, file_format))
 #for f in filelist:
 #  os.remove(f)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-led_pins= [13, 12, 18]
+def get_file_name():
+  out_dir = 'output'
+  prefix = 'dice-'
 
-for pin in led_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, GPIO.HIGH)
+  file_format = 'jpg'
+  imgs = glob.glob('{}/*.{}'.format(out_dir, file_format))
 
-subprocess.call([
-  'raspistill',
-  '--nopreview',
-  '-w', '1640',
-  '-h', '922',
-  '-t', '1',
-  '-ex', 'verylong',
-  '-cfx', '128:128',
-  '-awb', 'fluorescent',
-  '-mm', 'spot',
-  '-ISO', '800',
-  '--shutter', '25000',
-  '--contrast', '90',
-  '--brightness', '30',
-  '-o', out_name])
+  if len(imgs) == 0:
+    next_num = 0
+  else:
+    nums = [int(splitext(basename(img))[0].replace(prefix, '')) for img in imgs]
+    next_num = 1 + max(nums)
 
-print(out_name)
+  return '{}/{}{}.{}'.format(out_dir, prefix, str(next_num).zfill(4), file_format)
 
-for pin in led_pins:
-  GPIO.output(pin, GPIO.LOW)
 
-GPIO.cleanup()
+
+def capture():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setwarnings(False)
+  led_pins= [13, 12, 18]
+
+  for pin in led_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+  subprocess.call([
+    'raspistill',
+    '--nopreview',
+    '-w', '1640',
+    '-h', '922',
+    '-t', '1',
+    '-ex', 'verylong',
+    '-cfx', '128:128',
+    '-awb', 'fluorescent',
+    '-mm', 'spot',
+    '-ISO', '800',
+    '--shutter', '25000',
+    '--contrast', '90',
+    '--brightness', '30',
+    '-o', get_file_name()])
+
+  print(out_name)
+
+  for pin in led_pins:
+    GPIO.output(pin, GPIO.LOW)
+
+  GPIO.cleanup()

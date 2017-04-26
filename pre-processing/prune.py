8b805ef8c8f11ec4f91b60c3b8589347ea133c2b
Arthur Carabott <arthurc@gmail.com>
2017-04-26 11:16:35 +0100

    prune: get rid of files smaller than thresh

---
+++ pre-processing/prune.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import glob
+from os.path import getsize
+import shutil
+import argparse
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('in_dir', help='path to input image', type=str)
+  parser.add_argument('out_dir', help='directory to output results', type=str)
+  parser.add_argument('--thresh', help='file size threshold (bytes), only keep files >=',
+                      type=int, default=1600)
+  args = parser.parse_args()
+
+  for i, file in enumerate(glob.glob('{}/*'.format(args.in_dir))):
+    if getsize(file) >= args.thresh:
+      new_name = '{}.jpg'.format(str(i).zfill(5))
+      path = '{}/{}'.format(args.out_dir, new_name)
+      shutil.copyfile(file, path)
+
+
+if __name__ == '__main__':
+  main()

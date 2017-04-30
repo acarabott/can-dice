7e03ba4d718f71fa7f18c71b9768aadf2845169b
Arthur Carabott <arthurc@gmail.com>
2017-04-30 14:02:35 +0100

    minimising classify_image

--- tensorflow/code/classify_image.py
+++ tensorflow/code/classify_image.py
@@ -29,32 +29,15 @@ Please see the tutorial and website for a detailed description of how
 to use this script to perform image recognition.
 
 https://tensorflow.org/tutorials/image_recognition/
+
+Modified by Arthur Carabott 2017
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import argparse
-import os.path
-import re
-import sys
-
 import numpy as np
 import tensorflow as tf
 
 FLAGS = None
-
-# pylint: disable=line-too-long
-# pylint: enable=line-too-long
-
-def create_graph():
-  """Creates a graph from saved GraphDef file and returns a saver."""
-  # Creates graph from saved graph_def.pb.
-  with tf.gfile.FastGFile('{}/output_graph.pb'.format(FLAGS.model_dir), 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
 
 
 def run_inference_on_image(image):
@@ -62,18 +45,12 @@ def run_inference_on_image(image):
     tf.logging.fatal('File does not exist %s', image)
   image_data = tf.gfile.FastGFile(image, 'rb').read()
 
-  # Creates graph from saved GraphDef.
-  create_graph()
+  with tf.gfile.FastGFile('{}/output_graph.pb'.format(FLAGS.model_dir), 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
 
   with tf.Session() as sess:
-    # Some useful tensors:
-    # 'softmax:0': A tensor containing the normalized prediction across
-    #   1000 labels.
-    # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
-    #   float description of the image.
-    # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
-    #   encoding of the image.
-    # Runs the softmax tensor by feeding the image_data as input to the graph.
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
     predictions = sess.run(softmax_tensor,
                            {'DecodeJpeg/contents:0': image_data})
@@ -83,13 +60,12 @@ def run_inference_on_image(image):
 
     top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
     for node_id in top_k:
-      # human_string = node_lookup.id_to_string(node_id)
       human_string = lookup[node_id]
       score = predictions[node_id]
       print('%s (score = %.5f)' % (human_string, score))
 
 
-def main(_):
+def main():
   run_inference_on_image(FLAGS.image_file)
 
 
@@ -109,8 +85,9 @@ if __name__ == '__main__':
   parser.add_argument(
       '--num_top_predictions',
       type=int,
-      default=5,
+      default=1,
       help='Display this many predictions.'
   )
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
+  main()

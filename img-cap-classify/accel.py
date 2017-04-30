563190a61b5983737d0b0ab875d6bead6836164a
Arthur Carabott <arthurc@gmail.com>
2017-04-30 17:57:05 +0100

    classify accel listener started, thread started

---
+++ img-cap-classify/accel.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+# import smbus
+import time
+# import cap
+import threading
+
+
+# bus = smbus.SMBus(1)
+# address = 0x6b
+
+# OUT_X_L_XL = 0x28
+# OUT_X_H_XL = 0x29
+# OUT_Y_L_XL = 0x2A
+# OUT_Y_H_XL = 0x2B
+# OUT_Z_L_XL = 0x2C
+# OUT_Z_H_XL = 0x2D
+
+# # bus.write_byte_data(address, 0x10, 0xfb)  # enable accell and gyro
+# # Linear acceleration sensor control register 6
+# # enable accelerometer @ 50hz
+# # 0 1 0  0 0  0  0 0 = 0x40
+# CTRL_REG6_XL = 0x20
+# bus.write_byte_data(address, CTRL_REG6_XL, 0x40)
+
+
+# def read_i2c_word(register):
+#   low = bus.read_byte_data(address, register)
+#   high = bus.read_byte_data(address, register + 1)
+
+#   value = (high << 8) + low
+
+#   if (value >= 0x8000):
+#     value = -((65535 - value) + 1)
+
+#   return value / 32768.0
+
+
+# def getv(reg):
+#   return round(read_i2c_word(reg), 3)
+
+
+# def getx():
+#   return getv(OUT_X_L_XL)
+
+
+# def gety():
+#   return getv(OUT_Y_L_XL)
+
+
+# def getz():
+#   return getv(OUT_Z_L_XL)
+
+
+# x = px = getx()
+# y = px = gety()
+# z = pz = getz()
+
+# d = pd = 0
+# moving = False
+
+running = True
+thread = None
+
+
+def loop():
+  while running:
+    print('running')
+    time.sleep(1)
+
+
+def start():
+  print("started accelerometer listener")
+  global thread, running
+  thread = threading.Thread(target=loop)
+  running = True
+  thread.start()
+
+
+def stop():
+  global thread, running
+  print('stopped accelerometer listener')
+  running = False
+  thread.join()
+
+
+# img_count = 0
+# while True:
+#   # store previous values
+#   px = x
+#   py = y
+#   pz = z
+#   pd = d
+
+#   # get new values
+#   x = getx()
+#   y = gety()
+#   z = getz()
+
+#   # calculate the amount of change
+#   dx = (x - px) ** 2
+#   dy = (y - py) ** 2
+#   dz = (z - pz) ** 2
+
+#   # get total change, filtered
+#   d = dx + dy + dz
+#   m = 0.4  # was 0.3 for hand
+#   d = (d * m) + (pd * (1.0 - m))
+
+#   # decide if moving
+#   thresh = 0.05  # 0.05 for hand
+
+#   now_moving = d > thresh
+#   # print('{:04.2f}'.format(d), now_moving)
+#   # if now_moving and not moving:
+#   #   print("started VVVVVVV")
+#   #   print(d)
+
+#   if moving and not now_moving:
+#     if y > 0:
+#       cap.capture(img_count)
+#       img_count += 1
+#       print("snap!")
+#     # print("stopped -------")
+
+#   moving = now_moving
+#   time.sleep(0.05)

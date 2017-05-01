f04e2322586115d1e9762f12bc47885a2bb3e092
Arthur Carabott <arthurc@gmail.com>
2017-05-01 22:59:29 +0100

    server: saving and logging

--- production/server/server.py
+++ production/server/server.py
@@ -3,6 +3,7 @@ from flask import request
 from Brain import Brain
 import atexit
 import datetime
+import time
 import io
 import dice_processing
 import os
@@ -23,6 +24,27 @@ def allowed_file(filename):
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def save_image(file):
+  date_str = datetime.datetime.utcnow().strftime('%Y%m%d-%H%M%S-%f')
+  file_name = os.path.join(UPLOAD_FOLDER, '{}.jpg'.format(date_str))
+  file.save(file_name)
+
+
+def classify_image(file):
+  cropped = dice_processing.crop_dice(file)
+  results = []
+
+  for img in cropped:
+    with io.BytesIO() as output:
+       img.convert('RGB').save(output, 'JPEG')
+       data = output.getvalue()
+       result = brain.classify(data)
+       results.append(str(result[0]))
+
+  result = ''.join(str(i) for i in results)
+  return result
+
+
 @app.route('/')
 def index():
   return "POST a jpeg image with file key 'img' to /classify"
@@ -40,23 +62,9 @@ def classify():
       return 'empty file posted'
 
     if file and allowed_file(file.filename):
-      date_str = datetime.datetime.utcnow().strftime('%Y%m%d-%H%M%S-%f')
-
-      file_name = os.path.join(UPLOAD_FOLDER, '{}.jpg'.format(date_str))
-      file.save(file_name)
-
-      cropped = dice_processing.crop_dice(file)
-      results = []
-
-      for img in cropped:
-        with io.BytesIO() as output:
-           img.convert('RGB').save(output, 'JPEG')
-           data = output.getvalue()
-           result = brain.classify(data)
-           results.append(str(result[0]))
-
-      result = ''.join(str(i) for i in results)
-      print(result)
+      save_image(file)
+      result = classify_image(file)
+      print('{} - {}'.format(result, time.asctime()))
       return result
 
   else:
